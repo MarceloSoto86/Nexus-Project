fcecb9b3d9f490281c7863a8cdd07f4898a5f49f
Marcelo Soto <92414181+MarceloSoto86@users.noreply.github.com>
2026-04-22 22:49:48 +0100

    18th commit - Main Menu UI added + button scripts

---
+++ Nexus/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main Level");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+}
